e3b9f43c8a6259878716c81ac7d3e8abd83b9382
Erick Lopes <30758033+VindeeR@users.noreply.github.com>
2018-10-06 17:38:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.post('/send', (req, res) => {
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: 'k3rsucces@gmail.com', // generated ethereal user
-            pass: 'lfs123!@#' // generated ethereal password
+            user: '', // generated ethereal user
+            pass: '' // generated ethereal password
         },
         tls:{
             rejectUnauthorized:false
